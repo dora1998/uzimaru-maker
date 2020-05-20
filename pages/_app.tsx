5a2ea55be4fde7446290b0c86b88f02835202fe0
dora <31735614+dora1998@users.noreply.github.com>
2020-05-20 19:12:25 +0900

    chore: Setup chakra-ui

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { appTheme } from '../lib/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={appTheme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
