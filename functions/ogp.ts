abb971f2054baec6311ed4cc075767b4a46aa02c
dora <31735614+dora1998@users.noreply.github.com>
2020-06-01 06:03:01 +0900

    fix: 関数の返り値の型を追加

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -3,7 +3,7 @@ import { createCanvas, registerFont, Image } from 'canvas'
 import path from 'path'
 import { appTheme } from '../lib/theme'
 
-const loadImage = async (src: string) => {
+const loadImage = (src: string): Promise<Image> => {
   const img = new Image()
   return new Promise((resolve, reject) => {
     img.onload = () => resolve(img)
@@ -14,7 +14,10 @@ const loadImage = async (src: string) => {
   })
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
   registerFont(path.join('fonts', 'mplus-1c-medium.ttf'), {
     family: 'mplus',
   })
