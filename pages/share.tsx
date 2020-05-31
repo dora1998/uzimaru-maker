2f3938cd25c410f5261161623acacf9406ee922f
dora <31735614+dora1998@users.noreply.github.com>
2020-06-01 04:21:30 +0900

    feat: /shareページを作成

---
+++ pages/share.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+const SITE_URL = process.env.SITE_URL
+
+interface ServerSideProps {
+  result: string | string[]
+}
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (
+  ctx
+) => {
+  return { props: { result: ctx.query.result } }
+}
+
+type Props = ServerSideProps
+const Share: NextPage<Props> = ({ result }) => {
+  const ogpUrl = `${SITE_URL}/api/ogp?result=${result}`
+
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/')
+  }, [])
+
+  return (
+    <Head>
+      <meta property="og:image" content={ogpUrl} />
+      <meta name="twitter:image" content={ogpUrl} />
+    </Head>
+  )
+}
+export default Share
