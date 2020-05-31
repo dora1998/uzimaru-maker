127428e65f3746708bbd57f835abea76a11d03fb
dora <31735614+dora1998@users.noreply.github.com>
2020-06-01 06:02:10 +0900

    feat: 仮でOGPデザインを作成

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -1,8 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas, registerFont } from 'canvas'
+import { createCanvas, registerFont, Image } from 'canvas'
 import path from 'path'
+import { appTheme } from '../lib/theme'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const loadImage = async (src: string) => {
+  const img = new Image()
+  return new Promise((resolve, reject) => {
+    img.onload = () => resolve(img)
+    img.onerror = (err) => {
+      reject(err)
+    }
+    img.src = src
+  })
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   registerFont(path.join('fonts', 'mplus-1c-medium.ttf'), {
     family: 'mplus',
   })
@@ -10,9 +22,19 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   const canvas = createCanvas(960, 540)
   const context = canvas.getContext('2d')
 
+  context.fillStyle = appTheme.colors.uzimaru.gray
+  context.fillRect(0, 0, 960, 540)
+
+  const uzimaru = await loadImage('./public/images/uzimaru.svg')
+  context.drawImage(uzimaru, 0, 0)
+
+  context.fillStyle = '#fff'
+  context.font = '144px mplus'
+  context.fillText('100', 500, 300)
   context.font = '72px mplus'
-  context.fillStyle = '#ffffff'
-  context.fillText('OGPのテスト', 100, 100)
+  context.fillText('点', 800, 290)
+  context.font = '48px mplus'
+  context.fillText('のうじまるくん', 500, 400)
 
   const image = canvas.toBuffer()
 
